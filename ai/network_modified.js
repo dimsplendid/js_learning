f8917d8dd9657def642b23c65122713cb65c4bd4
andrew.tseng <andrew.tseng@innolux.com>
2024-10-23 10:07:23 +0800

    Modified document

--- ai/network_modified.js
+++ ai/network_modified.js
@@ -3,27 +3,22 @@ var nj = require('numjs');
 /**
  * Initialize a neural networks weights and bias with random number.
  * @param {number[]} layers - The number of neuron for each layer(include input)
- * @returns {object} The object constain weights(w) and bias(b)
+ * @returns {{w: NdArray[], b: NdArray[]}} The neural networks constain weights(w) and bias(b)
  * @example
  * // initialize a neural network
  * const nn = nn_init([3,2,1]);
  * // nn = {w:[...], b:[...]}
  */
 function nn_init(layers) {
-    return {
-        w: layers
-            .slice(0, layers.length-1)
-            .map((e,i) => [layers[i+1], e])
-            .map(wi => nj.random(wi[0], wi[1])),
-        b: layers
-            .slice(1)
-            .map(bi => nj.random(bi, 1))
-    }
+    const weights = layers.slice(0,-1)
+        .map((_,i) => nj.random(layers[i+1], layers[i]));
+    const biases = layers.slice(1).map(b => nj.random(b, 1));
+    return {w: weights, b: biases}
 }
 
 /**
  * 
- * @param {object} nn - neural network
+ * @param {{w: NdArray[], b: NdArray[]}} nn - neural network
  * @param {NdArray} input - shape(N, 1) ndarray, N should be
  *                           the same with nn.w[layer] size.
  * @param {boolean} log - log to record result and activation for each layer
@@ -34,23 +29,24 @@ function nn_init(layers) {
  * let {predict, log} = nn_forward(nn, input)
  */
 function nn_forward(nn, input, log=false) {  
-    
-    let tmp_log = null
-    if (log) tmp_log = {zs:[], as:[input]};
 
-    const forward = (nn, input, tmp_log=null, layer=0) => {
-        if (layer === nn.w.length) return input;
-        let z = nn.w[layer].dot(input).add(nn.b[layer])
-        let a = nj.sigmoid(z);
-        if (tmp_log !== null) {
-            tmp_log.zs.push(z);
-            tmp_log.as.push(a);
-        }
-        return forward(nn, a, tmp_log, layer+1)
+    const forward = (nn, input, log_data=null, layer=0) => {
+        if (layer === nn.w.length) return [input, log_data];
+
+        const z = nn.w[layer].dot(input).add(nn.b[layer]);
+        const a = nj.sigmoid(z);
+
+        const update_log = log_data ? {
+            zs: [...log_data.zs, z],
+            as: [...log_data.as, a]
+        } : null;
+
+        return forward(nn, a, update_log, layer+1)
     }
 
-    let predict = forward(nn, input, tmp_log)
-    return {predict: predict, log: tmp_log}
+    const initial_log = log ? {zs: [], as: [input]} : null;
+    const [predict, final_log] = forward(nn, input, initial_log)
+    return {predict, log: final_log}
 }
 
 /**
@@ -62,17 +58,29 @@ function dsigmoid(x) {
     return nj.sigmoid(x).multiply(nj.ones(x.shape).subtract(nj.sigmoid(x)));
 }
 
+/**
+ * Calculate nabla of neural network using backward propagation algorithm
+ * @param {{w: NdArray[], b: NdArray[]}} nn - neural network
+ * @param {NdArray} input - shape(N, 1) ndarray, N should be
+ *                           the same with nn.w[layer] size.
+ * @param {NdArray} target - shape(M, 1) ndarray, the one-hot
+ *                           encoding result
+ * @returns {{w: NdArray[], b: NdArray[]}} the nabla w and b of the neural network
+ * 
+ * @example
+ * let nabla = nn_backprop(nn, input, target);
+ * nn = nn_update(nn, nabla, eta=0.01);
+ */
 function nn_backprop(nn, input, target) {
     let nabla_w = nn.w.map(wi => nj.zeros(wi.shape));
     let nabla_b = nn.b.map(bi => nj.zeros(bi.shape));
 
-    let {log} = nn_forward(nn, input, true);
-    let {zs, as} = log;
-    let delta = nj.multiply(as.at(-1).subtract(target), dsigmoid(zs.at(-1)));
+    const { log } = nn_forward(nn, input, true);
+    const { zs, as } = log;
+    const delta = nj.multiply(as.at(-1).subtract(target), dsigmoid(zs.at(-1)));
 
     nabla_w = nabla_w.with(-1, delta.dot(as.at(-2).T));
     nabla_b = nabla_b.with(-1, delta);
-
 
     for (let l = 2; l <= nn.w.length; l++) {
         let z = zs.at(-l);
@@ -85,31 +93,49 @@ function nn_backprop(nn, input, target) {
     return {w: nabla_w, b: nabla_b};
 }
 
+/**
+ * Update neural network's weights by nabla
+ * @param {{w: NdArray[], b: NdArray[]}} nn - neural network
+ * @param {{w: NdArray[], b: NdArray[]}} nabla - the nabla of the neural network
+ * @param {number} eta - learning rate
+ * @returns {{w: NdArray[], b: NdArray[]}} weight-updated neural network
+ * @example
+ * let nabla = nn_backprop(nn, input, target);
+ * nn = nn_update(nn, nabla, eta=0.01);
+ */
 function nn_update(nn, nabla, eta) {
-    nn.w = nn.w.map((wi, i) => wi.subtract(nabla.w[i].multiply(eta)));
-    nn.b = nn.b.map((bi, i) => bi.subtract(nabla.b[i].multiply(eta)));
+    const updated_weights = nn.w.map((wi, i) => wi.subtract(nabla.w[i].multiply(eta)));
+    const updated_biases = nn.b.map((bi, i) => bi.subtract(nabla.b[i].multiply(eta)));
 
-    return nn
+    return {w: updated_weights, b: updated_biases};
 }
 
+/**
+ * Calculate the distance sqaure between 2 vector
+ * @param {NdArray} v1 - vector 1
+ * @param {NdArray} v2 - vector 2
+ * @returns {number} the distance sqaure of v1 and v2
+ */
 function dist_square(v1, v2) {
     let delta = v1.subtract(v2);
     return nj.sum(delta.T.dot(delta));
 }
-// test main
-let nn = nn_init([4,2,3]);
-let input = nj.array([1, 0, 0, 0]).reshape([4,1]);
-let target = nj.array([0,0,1]).reshape([3,1])
 
-// test train
-let count = 0;
-let eta = 0.1;
-while (count < 10000) {
-    if (count % 100 === 0) {
-        let {predict} = nn_forward(nn, input);
-        console.log(`The distance to target: ${dist_square(predict, target)}`)
-    }
-    nabla = nn_backprop(nn, input, target);
-    nn = nn_update(nn, nabla, eta);
-    count++;
-}
+// // test main
+// let nn = nn_init([4,2,3]);
+// let input = nj.array([1, 0, 0, 0]).reshape([4,1]);
+// let target = nj.array([0,0,1]).reshape([3,1])
+
+// // test training
+// let count = 0;
+// let eta = 0.1;
+// while (count < 10000) {
+//     if (count % 100 === 0) {
+//         let {predict} = nn_forward(nn, input);
+//         console.log(`The distance to target: ${dist_square(predict, target)}`)
+//     }
+//     nabla = nn_backprop(nn, input, target);
+//     nn = nn_update(nn, nabla, eta);
+//     count++;
+// }
+
